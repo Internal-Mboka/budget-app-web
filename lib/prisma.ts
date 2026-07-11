854d8a8537117dfa7346e32ae2a01666e083a521
vanguleprince <vangu.leprince@gmail.com>
2026-07-12 00:19:30 +0100

    fix / update du lancement via vercel 9eme tentative

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,8 @@
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
 
-const connectionString = process.env.DATABASE_URL;
-
-if (!connectionString) {
-  throw new Error("DATABASE_URL is not defined.");
-}
+const fallbackDatabaseUrl = "postgresql://user:pass@localhost:5432/db";
+const connectionString = process.env.DATABASE_URL ?? fallbackDatabaseUrl;
 
 const adapter = new PrismaNeon({ connectionString });
 
